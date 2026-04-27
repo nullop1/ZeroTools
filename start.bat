551d8e740134ec7677d692938137cdd8edacd074
TheNull <sobhanupmm@gmail.com>
2026-04-27 15:16:27 +0330

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,18 @@
+@echo off
+echo If your internet is restricted, consider using a mirror: runflare.com/mirrors
+echo Upgrading Python PIP...
+python -m pip install --upgrade pip
+echo Installing Python libraries from requirements.txt...
+pip install -r requirements.txt
+
+if errorlevel 1 (
+    echo Failed to install some libraries.
+    echo Try using a mirror: runflare.com/mirrors
+    pause
+    exit /b
+)
+
+echo Libraries installed successfully!
+echo Running main.py...
+python main.py
+pause
